b88a067e1b2cc181dce090196a7bc28ac29b4df2
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2025-09-09 13:04:52 +0300

    feat: update AppModule to include UsersModule and OrdersModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [PrismaModule, ProductsModule],
+  imports: [PrismaModule, ProductsModule, UsersModule, OrdersModule],
 })
 export class AppModule {}
