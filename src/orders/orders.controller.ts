3639cf5d2bf7d108005322a7faf2c637e5c3a050
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2025-09-09 13:03:35 +0300

    feat: add OrdersController with CRUD endpoints

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Post, Get, Delete, Param, Body, ParseIntPipe } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { CreateOrderDto } from './dto/create-order.dto';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @Post()
+  create(@Body() dto: CreateOrderDto) {
+    return this.ordersService.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.ordersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.ordersService.findOne(id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.ordersService.remove(id);
+  }
+}
