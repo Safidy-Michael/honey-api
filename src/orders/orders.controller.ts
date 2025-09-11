f8a33d8326bacfa571c8888dc83c4b5ada3d0897
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2025-09-11 10:48:57 +0300

    chore: update status

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Get, Delete, Param, Body, ParseIntPipe } from '@nestjs/common';
+import { Controller, Post, Get, Delete, Param, Body, ParseIntPipe, Put } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderStatusDto } from './dto/create-order.dto';
 
 @Controller('orders')
 export class OrdersController {
@@ -20,6 +21,11 @@ export class OrdersController {
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.ordersService.findOne(id);
   }
+  
+   @Put(':id')
+update(@Param('id', ParseIntPipe) id: number, @Body() dto: Partial<CreateOrderDto> & { status?: string }) {
+  return this.ordersService.update(id, dto);
+}
 
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
