b170b4584c6d94e54581a340db3c6b47737663e5
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2025-09-11 12:19:30 +0300

    fix:update status réservé à l’admin + exceptions

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Post, Get, Delete, Param, Body, ParseIntPipe, Put } from '@nestjs/common';
+import { Controller, Post, Get, Delete, Param, Body, ParseIntPipe, Put, ForbiddenException, Req } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderStatusDto } from './dto/create-order.dto';
+import { Request } from 'express';
 
 @Controller('orders')
 export class OrdersController {
@@ -21,11 +21,20 @@ export class OrdersController {
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.ordersService.findOne(id);
   }
-  
-   @Put(':id')
-update(@Param('id', ParseIntPipe) id: number, @Body() dto: Partial<CreateOrderDto> & { status?: string }) {
-  return this.ordersService.update(id, dto);
-}
+
+  @Put(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: Partial<CreateOrderDto> & { status?: string },
+    @Req() req: Request
+  ) {
+    const user = req.user as { role?: string }; 
+    if (dto.status && user?.role !== 'admin') {
+      throw new ForbiddenException('Seul l’admin peut modifier le statut de la commande.');
+    }
+
+    return this.ordersService.update(id, dto);
+  }
 
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
