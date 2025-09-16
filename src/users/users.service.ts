41a3fa461b6487d7823d81cf8251cbbeb40bd821
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2025-09-16 07:35:17 +0300

    chore: create admin

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,27 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt'; 
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  create(createUserDto: CreateUserDto) {
-    return this.prisma.user.create({ data: createUserDto });
+  async create(createUserDto: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    return this.prisma.user.create({
+      data: {
+        ...createUserDto,
+        password: hashedPassword,
+        role: 'client', 
+      },
+    });
   }
 
   findAll() {
     return this.prisma.user.findMany();
   }
 
-   findOne(id: number) {
+  findOne(id: number) {
     if (!id) {
-      throw new Error('L\'id de l\'utilisateur est requis');
+      throw new Error("L'id de l'utilisateur est requis");
     }
     return this.prisma.user.findUnique({
-      where: { id: id },
+      where: { id },
     });
   }
+
   findByEmail(email: string) {
     return this.prisma.user.findUnique({ where: { email } });
   }
@@ -32,5 +41,16 @@ export class UsersService {
 
   remove(id: number) {
     return this.prisma.user.delete({ where: { id } });
+  }
+
+  async createAdmin(createUserDto: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    return this.prisma.user.create({
+      data: {
+        ...createUserDto,
+        password: hashedPassword,
+        role: 'admin', 
+      },
+    });
   }
 }
