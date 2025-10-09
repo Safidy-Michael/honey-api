a2e942c1ba95ad2c382cd132bcdc08cb1bfd2741
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2025-10-09 09:47:12 +0300

    fix: fixing bug on hashing password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,11 +8,10 @@ export class UsersService {
   constructor(private prisma: PrismaService) {}
 
   async create(createUserDto: CreateUserDto) {
-    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    
     return this.prisma.user.create({
       data: {
         ...createUserDto,
-        password: hashedPassword,
         role: 'client', 
       },
     });
