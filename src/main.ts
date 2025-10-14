2c2b65b193880b5028ee4cded6a18fa430448176
Michael <randriasafidy.77@gmail.com>
2025-10-14 10:10:13 +0300

    Update cors config

--- src/main.ts
+++ src/main.ts
@@ -4,38 +4,33 @@ import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as express from 'express';
-import { Request, Response, NextFunction } from 'express'; 
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  const allowedOrigins = [
+    'http://localhost:8080', 
+    'https://honey-bloom-frontend.onrender.com', 
+  ];
+
   app.enableCors({
-    origin: [
-      'https://honey-bloom-frontend.onrender.com',  
-    ],
+    origin: allowedOrigins,
     credentials: true,
-    methods: ['GET','HEAD','PUT','PATCH','POST','DELETE','OPTIONS'],
+    methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
     allowedHeaders: ['Content-Type','Authorization'],
   });
 
   
-  app.use(express.json());
-  app.use((req: Request, res: Response, next: NextFunction) => {
-    if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Origin', 'https://honey-bloom-frontend.onrender.com');
-      res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-      return res.sendStatus(204);
-    }
-    next();
-  });
-
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
   });
 
+ 
+  app.use(express.json());
+
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT);
+
   console.log(`Server is running on port ${PORT}`);
 }
 
